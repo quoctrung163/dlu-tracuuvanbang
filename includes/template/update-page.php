9aa566afe995606efcd0fa8aef4d986edb0c1ff3
La Quốc Thắng <quocthang0507@gmail.com>
2020-08-13 14:08:27 +0700

    update print statement

--- includes/template/update-page.php
+++ includes/template/update-page.php
@@ -1,10 +1,6 @@
 <?php
 
-// Load dữ liệu từ excel
-// include '../../lib/excelData.php';
-// require_once('../../lib/excelData.php');
-
 $file = ABSPATH . "/wp-content/plugins/dlu-tracuuvanbang/lib/excelData.php";
 // echo ABSPATH;
 require($file);
-echo $data;
+var_dump($data);
