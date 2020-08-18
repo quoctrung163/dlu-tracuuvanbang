51085bb57bb8593b06a52cf71edfa62af17de6e4
La Quốc Thắng <quocthang0507@gmail.com>
2020-08-18 17:07:02 +0700

    Update files

--- includes/template/menu-view-page.php
+++ includes/template/menu-view-page.php
@@ -5,5 +5,5 @@ require_once($file1);
 
 echo '<h1 class="myplugin">Xem danh sách văn bằng học viên</h1>';
 echo '<h3 class="myplugin">Dưới đây là toàn bộ thông tin văn bằng của các học viên trong trung tâm và được lưu trữ trong cơ sở dữ liệu</h3>';
-$data = getHocVienDaHoanThanh();
+// $data = getHocVienDaHoanThanh();
 printTable($data);
